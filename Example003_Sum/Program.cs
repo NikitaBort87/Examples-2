3c62afc94b469761d2d3137798f02df8406ac520
NIKITA <Nikita.bort87@yandex.ru>
2023-03-09 22:12:58 +0300

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numbrerB =5;
-int result = numberA + numbrerB;
+﻿int numberA = new Random().Next(1,10);
+System.Console.WriteLine(numberA);
+int numberB =new Random().Next(1,10);
+System.Console.WriteLine(numberB);
+int result = numberA + numberB;
 System.Console.WriteLine(result);
