77277178c321b585b78dd7b77cfef132a4fb1d96
NIKITA <Nikita.bort87@yandex.ru>
2023-03-09 22:31:25 +0300

    add Example005_ifElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша! ");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
